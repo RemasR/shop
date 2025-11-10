f8d4aba33cc678cf0f17daa05190f1271ebc2369
RemasR <remas_xl@hotmail.com>
2025-11-10 12:32:49 +0300

    feat: create usecase for product entity

--- shop-mngt-domain/src/main/java/com/example/shop/domain/usecase/product/UpdateProductUsecase.java
+++ shop-mngt-domain/src/main/java/com/example/shop/domain/usecase/product/UpdateProductUsecase.java
@@ -1,4 +1,31 @@
 package com.example.shop.domain.usecase.product;
 
+import com.example.shop.domain.dto.ProductDTO;
+import com.example.shop.domain.entity.Product;
+import com.example.shop.domain.repository.ProductRepository;
+
 public class UpdateProductUsecase {
+    private final ProductRepository productRepository;
+    public UpdateProductUsecase(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+    public Product execute(int id, ProductDTO dto){
+        if(dto == null){
+            throw new IllegalArgumentException("Update data cannot be null");
+        }
+        Product existingProduct = productRepository.findById(id);
+
+        if(dto.getName() != null){
+            existingProduct.setName(dto.getName());
+        }
+
+        if(dto.getDescription() != null){
+            existingProduct.setDescription(dto.getDescription());
+        }
+
+        if(dto.getPrice() != null){
+            existingProduct.setPrice(dto.getPrice());
+        }
+        return productRepository.save(existingProduct);
+    }
 }
