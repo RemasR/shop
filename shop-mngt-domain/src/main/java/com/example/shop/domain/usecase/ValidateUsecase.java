593c6a7ed8c7fb804b7277b08bd9999e051e1835
RemasR <remas_xl@hotmail.com>
2025-11-11 15:18:49 +0300

    chore: for future implementation

---
+++ shop-mngt-domain/src/main/java/com/example/shop/domain/usecase/ValidateUsecase.java
@@ -0,0 +1,17 @@
+package com.example.shop.domain.usecase;
+
+import com.example.shop.domain.dto.SimpleViolation;
+import com.example.shop.domain.validators.Validator;
+
+import java.util.List;
+import java.util.Set;
+
+public class ValidateUsecase<T> {
+   /* public Set<SimpleViolation> violations(List<Validator<T>> validators) {
+        for (Validator<T> validator : validators){
+            validator.validate(T);
+        }
+    }
+
+    */
+}
--- shop-mngt-domain/src/main/java/com/example/shop/domain/validators/order/OrderDuplicateValidator.java
+++
@@ -1,4 +0,0 @@
-package com.example.shop.domain.validators.order;
-
-public class OrderDuplicateValidator {
-}
