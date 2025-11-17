d32117215ab7ba4bd47fae0070531e3fe73135c7
RemasR <remas_xl@hotmail.com>
2025-11-17 11:26:46 +0300

    feat: testing controller

--- shop-mngt-application/src/main/java/com/example/shop/controller/UserController.java
+++ shop-mngt-application/src/main/java/com/example/shop/controller/UserController.java
@@ -1,6 +1,19 @@
 package com.example.shop.controller;
 
+import com.example.shop.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private UserService userService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
 }
 /*
 POST   /api/users              - Create user
@@ -8,5 +21,4 @@ GET    /api/users/{id}         - Get user by ID
 GET    /api/users              - Get all users
 PUT    /api/users/{id}         - Update user
 DELETE /api/users/{id}         - Delete user
-GET    /api/users/email/{email} - Get user by email
  */
