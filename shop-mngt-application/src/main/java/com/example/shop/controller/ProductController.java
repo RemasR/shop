099fcf00ddbe5e2b1b65f71234c0aa6a7dc09b8a
RemasR <remas_xl@hotmail.com>
2025-11-18 12:27:13 +0300

    feat: implement product CRUD controllers

--- shop-mngt-application/src/main/java/com/example/shop/controller/ProductController.java
+++ shop-mngt-application/src/main/java/com/example/shop/controller/ProductController.java
@@ -1,11 +1,33 @@
 package com.example.shop.controller;
 
+import com.example.shop.domain.dto.ProductDTO;
+import com.example.shop.domain.entity.Product;
+import com.example.shop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public Product createOrder(@RequestBody ProductDTO dto) {
+        return productService.createProduct(dto);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable String id) {
+        return productService.getProductById(id);
+    }
+
+    public Product updateProduct(@PathVariable String id, @RequestParam ProductDTO dto) {
+        return productService.updateProduct(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+    }
 }
-/*
-POST   /api/products           - Create product
-GET    /api/products/{id}      - Get product by ID
-GET    /api/products           - Get all products
-PUT    /api/products/{id}      - Update product
-DELETE /api/products/{id}      - Delete product
- */
